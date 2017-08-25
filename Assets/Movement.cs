eeededf5cf072420e70cb9ccc504041faf2e75b7
LAPTOP-UHC40UK6\chand <chosteler@mail.pima.edu>
2017-08-24 23:52:51 -0700

    removed the box, added a sprite, and added  controler to the sprite to be able to move freely

---
+++ Assets/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float maxSpeed = 10f;
+    public Transform player;
+    public Rigidbody2D move;
+
+    // Use this for initialization
+
+
+    void Start () {
+        GameObject go = GameObject.Find("player");
+        move = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+    float moveUpDown = Input.GetAxis("Vertical");
+        move.velocity = new Vector2(moveUpDown * maxSpeed, move.velocity.x);
+        
+float moveRightLeft = Input.GetAxis("Horizontal");
+        move.velocity = new Vector2(moveRightLeft * maxSpeed, move.velocity.x);
+        
+	}
+}
